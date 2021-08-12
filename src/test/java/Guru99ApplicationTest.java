5ba7a530e53e80375d7e3933047f891269c5319e
Adam Pawtel <adam@pawtel.pl>
2021-08-12 12:46:42 +0200

    get page title and its test added

---
+++ src/test/java/Guru99ApplicationTest.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Guru99ApplicationTest {
+
+    ChromeDriver driver;
+
+    String url = "http://demo.guru99.com/v4/";
+
+    @BeforeMethod
+    public void invokeBrowser() {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium-drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get(url);
+    }
+
+    @Test
+    public void verifyTitleOfThePage() {
+
+        String expectedTitle = "Guru99 Bank Home Page";
+
+        String actualTitle = driver.getTitle();
+
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+}
